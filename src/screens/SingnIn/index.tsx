f78e54f4113234f2bb2bea4847e8df7fa4af73df
felipebrito-tech <felipe.brito87@gmail.com>
2021-06-28 14:10:36 -0300

    Metade da aula 02 | Início da Criação da Home até a inclusão da navegação

--- src/screens/SingnIn/index.tsx
+++ src/screens/SingnIn/index.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import {
     View,
     Text,
-    Image,
-    StatusBar
+    Image
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn(){
-  return(
+    const navigation = useNavigation();
+
+    function handleSignIn() {
+        navigation.navigate('Home');
+    }
+
+    return(
     <View style={styles.container}>
-        <StatusBar
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-        />
         <Image
             source={IllustrationImg}
             style={styles.image}
@@ -36,8 +37,11 @@ export function SignIn(){
                 favoritos com seus amigos
             </Text>
 
-            <ButtonIcon title='Entrar com o Discord' activeOpacity={0.7} />
+            <ButtonIcon
+                title='Entrar com o Discord'
+                onPress={handleSignIn}
+            />
         </View>
     </View>
-  );
+    );
 }
