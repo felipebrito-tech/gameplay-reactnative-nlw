61b619902eb55ee8385ca3d6ea8abf4855f266cf
felipebrito-tech <felipe.brito87@gmail.com>
2021-07-02 12:39:39 -0300

    Início da aula 03 | Ajustes de layout

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -7,6 +7,7 @@ import { CategorySelect } from '../../components/CategorySelect';
 import { ListHeader } from '../../components/ListHeader';
 import { ListDivider } from '../../components/ListDivider';
 import { Appointment } from '../../components/Appointment';
+import { Background } from '../../components/Background';
 
 import { styles } from './styles';
 
@@ -45,7 +46,7 @@ export function Home() {
     }
 
     return(
-        <View>
+        <Background>
             <View style={ styles.header }>
                 <Profile />
                 <ButtonAdd />
@@ -73,6 +74,6 @@ export function Home() {
                     showsVerticalScrollIndicator={false}
                 />
             </View>
-        </View>
+        </Background>
     );
 }
--- src/screens/SingnIn/index.tsx
+++ src/screens/SingnIn/index.tsx
@@ -6,7 +6,9 @@ import {
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
+import { Background } from '../../components/Background';
 import { ButtonIcon } from '../../components/ButtonIcon';
+
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
@@ -18,30 +20,32 @@ export function SignIn(){
     }
 
     return(
-    <View style={styles.container}>
-        <Image
-            source={IllustrationImg}
-            style={styles.image}
-            resizeMode="stretch"
-        />
+        <Background>
+            <View style={styles.container}>
+                <Image
+                    source={IllustrationImg}
+                    style={styles.image}
+                    resizeMode="stretch"
+                    />
 
-        <View style={styles.content}>
-            <Text style={styles.title}>
-                Conecte-se{'\n'}
-                e organize suas{'\n'}
-                jogatinas
-            </Text>
+                <View style={styles.content}>
+                    <Text style={styles.title}>
+                        Conecte-se{'\n'}
+                        e organize suas{'\n'}
+                        jogatinas
+                    </Text>
 
-            <Text style={styles.subtitle}>
-                Crie grupos para jogar seus games{'\n'}
-                favoritos com seus amigos
-            </Text>
+                    <Text style={styles.subtitle}>
+                        Crie grupos para jogar seus games{'\n'}
+                        favoritos com seus amigos
+                    </Text>
 
-            <ButtonIcon
-                title='Entrar com o Discord'
-                onPress={handleSignIn}
-            />
-        </View>
-    </View>
+                    <ButtonIcon
+                        title='Entrar com o Discord'
+                        onPress={handleSignIn}
+                    />
+                </View>
+            </View>
+        </Background>
     );
 }
