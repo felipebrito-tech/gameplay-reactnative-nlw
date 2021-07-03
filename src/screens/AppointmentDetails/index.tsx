362a4115ec9cd3bfe5a5d335bd81f7e6179a52a8
felipebrito-tech <felipe.brito87@gmail.com>
2021-07-03 11:32:45 -0300

    Aula 03, parte 02 | Criação da tela AppointmentDetails e do componente Header, com as navegações

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -5,13 +5,16 @@ import {
 } from 'react-native';
 
 import { Background } from '../../components/Background';
+import { Header } from '../../components/Header';
 
 import { styles } from './styles';
 
 export function AppointmentDetails(){
   return (
     <Background>
-
+      <Header
+        title="Detalhes"
+      />
     </Background>
   );
 }
